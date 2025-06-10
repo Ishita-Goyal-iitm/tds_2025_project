16f95b0cfef2e7b729e4245269e00c0dc35672a4
ishitagoyal0507 <ishitagoyal0507@gmail.com>
2025-06-10 16:11:22 +0530

    Add CORS middleware and enhance API endpoints for parameter retrieval

--- api/index.py
+++ api/index.py
@@ -1,8 +1,33 @@
 from fastapi import fastapi, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
-def index () : 
-    return {"message": "Welcome to the API!"}
+
+
+
+@app.get("/")
+def index (): 
+    return {
+        "message": "Welcome to the API!",
+        }
+
+@app.get("/api/params")
+def search (request: Request):
+    parameters = list()
+
+    for parameter_name in request.query_params.keys():
+        parameters.append(parameter_name)
+    print(parameters)        
+
+    return {
+        "parameters": parameters,
+    }
